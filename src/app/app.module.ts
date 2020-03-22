df0b4735fd5a64da1c967ef2593968dfe1aa6141
mougithub19 <moubusiness18@gmail.com>
2020-03-22 12:22:10 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChartModule } from '@syncfusion/ej2-ng-charts';
+import { StackingBarSeries, CategoryService} from '@syncfusion/ej2-ng-charts';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent],
+  providers: [ StackingBarSeries, CategoryService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
