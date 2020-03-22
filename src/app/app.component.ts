65fc4426d75b9fa708d6e5370bdc37a408379776
mougithub19 <moubusiness18@gmail.com>
2020-03-22 12:19:18 +0530

    3 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+ 
+    public data: Object[] =[
+                  { x: '2014', y: 111.1 },
+                  { x: '2015', y: 127.3 },
+                  { x: '2016', y: 143.4 },
+                  { x: '2017', y: 159.9 }];
+    public data1: Object[] =[
+                  { x: '2014', y: 76.9 },
+                  { x: '2015', y: 99.5 },
+                  { x: '2016', y: 121.7 },
+                  { x: '2017', y: 142.5 }];
+  
+  //Initializing Primary X Axis
+  public primaryXAxis: Object = {
+      valueType: 'Category',
+      
+  };
+
 }
